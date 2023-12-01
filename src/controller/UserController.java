48c6435c30c1e641b3257187c9b9c2f5155013a6
Andrea Viganò <viganoandrea02@gmail.com>
2023-12-01 16:46:15 +0800

    user controller impl draft

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -5,6 +5,9 @@ import controller.database.SQLManager;
 import model.User;
 
 import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class UserController extends Controller {
     private User user;
@@ -16,39 +19,71 @@ public class UserController extends Controller {
         String statement = sqlManager.getInsertStatement(
             "Address",
             new String[] { "address", "user_id" },
-            new String[] { address, this.user.getUserID()}
+            new String[] { address, this.user.getUserID() }
         );
-        printStream.println(statement);
-//        try {
-//            database.update(statement);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            database.update(statement);
+            database.commit();
+        } catch (SQLException e) {
+            database.abort();
+            return false;
+        }
         return true;
     }
 
     public boolean removeShippingAddress(String id) {
         String statement = sqlManager.getDeleteStatement("Address", "address_id=" + id);
-        printStream.println(statement);
-//        try {
-//            database.update(statement);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            database.update(statement);
+            database.commit();
+        } catch (SQLException e) {
+            database.abort();
+            return false;
+        }
         return true;
     }
 
-    public boolean showShippingAddresses() {
-        return true;
+    public void showShippingAddresses() {
+        String statement = sqlManager.getSelectStatement(
+                "Address",
+                new String[] { "name", "addressID" },
+                "userID = " + this.user.getUserId()
+        );
+        try {
+            ResultSet results = database.query(statement);
+            int i = 0;
+            while (results.next()) {
+                String address = results.getString("name");
+                String id = results.getString("addressID");
+                printStream.printf("%s. %s\n", id, address);
+                i++;
+            }
+            if (i == 0) { // No addresses printed
+                printStream.println("No address to show for current user");
+            }
+        } catch (SQLException ignored) { }
     }
 
     public boolean comparePasswords(String password) {
         String passwordHash = AuthenticationController.sha256(password);
-        String statement = "UPDATE Users ";
-        return true;
+        return Objects.equals(this.user.getPasswordHash(), passwordHash);
     }
 
     public boolean changePassword(String password) {
+        String passwordHash = AuthenticationController.sha256(password);
+        String statement = sqlManager.getUpdateStatement(
+                "Users",
+                new String[] { "passwordHash" },
+                new String[] { passwordHash },
+                "userID = " + this.user.getUserId()
+        );
+        try {
+            database.update(statement);
+            database.commit();
+        } catch (SQLException e) {
+            database.abort();
+            return false;
+        }
         return true;
     }
 
